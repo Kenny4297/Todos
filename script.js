1643c67a409bd38ae124d67fbc7f65da6b00acfd
Kenny4297 <geckob4i@gmail.com>
2024-03-02 11:47:06 -0600

    Fix event listener issue

--- script.js
+++ script.js
@@ -7,41 +7,47 @@ let todosContainer = document.querySelector(".container");
 // ? creating function to create unique id
 function uuid() {
     // universal unique identification
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (param) {
-        let number = Math.random() * 16 | 0,
-            randomNumber = param == 'x' ? number : (number & 0x3 | 0x8);
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (param) {
+        let number = (Math.random() * 16) | 0,
+            randomNumber = param == "x" ? number : (number & 0x3) | 0x8;
         return randomNumber.toString(16);
-    })
+    });
 }
 
 addToDoButton.addEventListener("click", (event) => {
     event.preventDefault();
-    // * Prevent default is used to prevent the form from doing the default action which is reloading the page after the button is clicked 
+    // * Prevent default is used to prevent the form from doing the default action which is reloading the page after the button is clicked
 
     todo = todoInputContainer.value;
     // * value is used to fetch the input field value which the user has enetered
     if (todoList.length >= 0 && todo.length > 0) {
         todoList.push({ id: uuid(), todo, isCompleted: false });
+        todoInputContainer.value = "";
     }
     renderTodoList(todoList);
-})
+});
 
 todosContainer.addEventListener("click", (event) => {
-    let key = event.target.dataset.key;
-    todoList = todoList.map((todo) => todo.id === key ? { ...todo, isCompleted: !todo.isCompleted } : todo)
-    console.log(event.target)
-    console.log(todoList)
-    
-})
-
+    // Ensure you're only responding to clicks on the checkbox
+    if (event.target.type === "checkbox") {
+        let key = event.target.dataset.key;
+        todoList = todoList.map((todo) => (todo.id === key ? { ...todo, isCompleted: !todo.isCompleted } : todo));
+        console.log(event.target);
+        console.log(todoList);
+    }
+});
 
 function renderTodoList(todoList) {
-    todosContainer.innerHTML = todoList.map(({ id, todo, isCompleted }) => 
-`<div class="item-container">
+    todosContainer.innerHTML = todoList
+        .map(
+            ({ id, todo, isCompleted }) =>
+                `<div class="item-container">
     <div class="wrapper">
         <input type="checkbox" data-key=${id} name=${todo} id=${id}>
         <label for=${id} data-key=${id}>${todo}</label>    
     </div>
     <img src="./trash.svg" alt="delete button">
-</div>`).join('')
+</div>`
+        )
+        .join("");
 }
