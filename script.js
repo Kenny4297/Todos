fc9fc10b5868889647259ba45503a7e065a3dce3
Rohit Saini <rohit.boby2004@gmail.com>
2024-03-02 14:43:45 +0530

    Added the files

---
+++ script.js
@@ -0,0 +1,63 @@
+let todoInputContainer = document.querySelector("#wishlist");
+let addToDoButton = document.querySelector(".button");
+let todo;
+let todoList = [];
+let todosContainer = document.querySelector(".container");
+
+// ? creating function to create unique id
+function uuid() {
+    // universal unique identification
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (param) {
+        let number = Math.random() * 16 | 0,
+            randomNumber = param == 'x' ? number : (number & 0x3 | 0x8);
+        return randomNumber.toString(16);
+    })
+}
+
+addToDoButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    // * Prevent default is used to prevent the form from doing the default action which is reloading the page after the button is clicked 
+
+    todo = todoInputContainer.value;
+    // * value is used to fetch the input field value which the user has enetered
+    if (todoList.length >= 0 && todo.length > 0) {
+        todoList.push({ id: uuid(), todo, isCompleted: false });
+    }
+    renderTodoList(todoList);
+})
+
+todosContainer.addEventListener("click", (event) => {
+    let key = event.target.dataset.key;
+    todoList = todoList.map(todo => todo.id === key ? { ...todo, isCompleted: !todo.isCompleted } : todo)
+    event.stopPropagation();
+    // alert(todoList)
+    console.log(event.target.dataset.key)
+    console.log(todoList)
+})
+
+
+function renderTodoList(todoList) {
+    todosContainer.innerHTML = todoList.map(({ id, todo, isCompleted }) => 
+`<div class="item-container">
+    <div class="wrapper">
+        <input type="checkbox" data-key=${id} name=${todo} id=${todo}>
+        <label for=${todo} data-key=${id}>${todo}</label>    
+    </div>
+    <img src="./trash.svg" alt="delete button">
+</div>`).join('')
+}
+
+
+// console.log(todoList.map((todo) => `
+// <div class="item-container">
+// <div class="wrapper">
+// <input type="checkbox" name=${todo.todo} id=${todo.todo}>
+// <label for=${todo.todo}>${todo.todo}</label>
+// </div>
+// <img src="./trash.svg" alt="delete button">
+// </div>`))
+
+// console.log(todosContainer.innerHTML)
+
+// let str = ["A", "B", "C"];
+// console.log(str.join('-'))
